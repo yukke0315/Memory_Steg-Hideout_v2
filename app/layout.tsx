be0a213582642806d3d8723e4cf43feb7ff280e6
yukke0315 <is0786rr@ed.ritsumei.ac.jp>
2026-05-03 19:07:25 +0900

    feat: add a link to the home in the title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +21,11 @@ export default function RootLayout({
         
         {/* 全画面共通のヘッダー */}
         <header className="w-full p-6 flex justify-between items-center border-b border-zinc-800">
-          <h1 className="text-l font-bold tracking-widest">
-            Memory <span className="text-zinc-500">Steg-Hideout#2</span>
-          </h1>
+          <Link href="/">
+            <h1 className="text-l font-bold tracking-widest">
+              Memory <span className="text-zinc-500">Steg-Hideout#2</span>
+            </h1>
+          </Link>
           
           <nav className="flex gap-6 text-sm font-medium text-zinc-400">
             <button className="hover:text-zinc-100 transition-colors">Create Hideout</button>
